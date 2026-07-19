1d5dad52108c19b56e1ab9f74d19e8e228ca907f
AndreiS <andrei.catalin.smeu@gmail.com>
2026-07-19 22:56:18 +0300

    added prettier to code format

--- pages/ProductPage.ts
+++ pages/ProductPage.ts
@@ -1,54 +1,71 @@
 import { Page, expect } from "@playwright/test";
 
-export class ProductPage{
+export class ProductPage {
+  readonly productCard: string;
+  readonly productName: string;
+  readonly productDescription: string;
+  readonly productPrice: string;
+  readonly filterDropdown: string;
+  readonly productPageName: string;
 
-    readonly productCard: string;
-    readonly productName: string;
-    readonly productDescription: string;
-    readonly productPrice: string;
-    readonly filterDropdown: string;
-    readonly productPageName: string;
+  constructor(private page: Page) {
+    this.productCard = ".inventory_item";
+    this.productName = ".inventory_item_name";
+    this.productDescription = ".inventory_item_desc";
+    this.productPrice = ".inventory_item_price";
+    this.filterDropdown = ".product_sort_container";
+    this.productPageName = ".inventory_details_name.large_size";
+  }
 
-    constructor(private page: Page){
-        this.productCard = '.inventory_item';
-        this.productName = '.inventory_item_name';
-        this.productDescription = '.inventory_item_desc';
-        this.productPrice = '.inventory_item_price'
-        this.filterDropdown = '.product_sort_container';
-        this.productPageName = '.inventory_details_name.large_size';
+  async verifyFirstProductName(productName: string) {
+    await expect(this.page.locator(this.productName).first()).toHaveText(
+      productName,
+    );
+  }
 
-    }
+  async verifyLastProductName(productName: string) {
+    await expect(this.page.locator(this.productName).last()).toHaveText(
+      productName,
+    );
+  }
 
-    async verifyFirstProductName(productName: string){
-        await expect(this.page.locator(this.productName).first()).toHaveText(productName);
-    }
+  async verifyProductNameFromList(productName: string, listNumber: number) {
+    listNumber = listNumber - 1;
+    await expect(
+      this.page.locator(this.productName).nth(listNumber),
+    ).toHaveText(productName);
+  }
 
-    async verifyLastProductName(productName: string){
-        await expect(this.page.locator(this.productName).last()).toHaveText(productName);
-    }
+  async filterProduct(filterOption: string) {
+    await this.page.selectOption(this.filterDropdown, filterOption);
+  }
 
-    async verifyProductNameFromList(productName: string, listNumber: number){
-        listNumber = listNumber - 1;
-        await expect(this.page.locator(this.productName).nth(listNumber)).toHaveText(productName);
-    }
+  async checkProductData(
+    productName: string,
+    productDescription: string,
+    productPrice: string,
+  ) {
+    const currentProduct = this.page
+      .locator(this.productCard)
+      .filter({ hasText: productName });
+    await expect(currentProduct.locator(this.productName)).toHaveText(
+      productName,
+    );
+    await expect(currentProduct.locator(this.productDescription)).toHaveText(
+      productDescription,
+    );
+    await expect(currentProduct.locator(this.productPrice)).toHaveText(
+      productPrice,
+    );
+  }
 
-    async filterProduct(filterOption: string){
-        await this.page.selectOption(this.filterDropdown, filterOption);
-    }
-
-    async checkProductData(productName: string, productDescription: string, productPrice: string){
-        const currentProduct = this.page.locator(this.productCard).filter({hasText: productName});
-        await expect(currentProduct.locator(this.productName)).toHaveText(productName);
-        await expect(currentProduct.locator(this.productDescription)).toHaveText(productDescription);
-        await expect(currentProduct.locator(this.productPrice)).toHaveText(productPrice);
-    }
-
-    async accessProductPage(prouctName: string){
-        const currentProduct = this.page.locator(this.productCard).filter({hasText: prouctName});
-        await currentProduct.locator(this.productName).click();
-        await expect(this.page.locator(this.productPageName)).toHaveText(prouctName);
-    }
-
-    
-
+  async accessProductPage(prouctName: string) {
+    const currentProduct = this.page
+      .locator(this.productCard)
+      .filter({ hasText: prouctName });
+    await currentProduct.locator(this.productName).click();
+    await expect(this.page.locator(this.productPageName)).toHaveText(
+      prouctName,
+    );
+  }
 }
--- tests/Login.test.ts
+++ tests/Login.test.ts
@@ -1,11 +1,18 @@
-import {test} from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
+import { test } from "@playwright/test";
+import { LoginPage } from "../pages/LoginPage";
 
-test('Open Test Page', async ({ page }) => {
-   
-    const loginPageActions = new LoginPage(page);
+test("Open Test Page", async ({ page }) => {
+  const loginPageActions = new LoginPage(page);
 
-    await loginPageActions.accessSite();
-    await loginPageActions.invalidLogin('standard_user','1234','Epic sadface: Username and password do not match any user in this service');
-    await loginPageActions.invalidLogin('locked_out_user','secret_sauce','Epic sadface: Sorry, this user has been locked out.');
+  await loginPageActions.accessSite();
+  await loginPageActions.invalidLogin(
+    "standard_user",
+    "1234",
+    "Epic sadface: Username and password do not match any user in this service",
+  );
+  await loginPageActions.invalidLogin(
+    "locked_out_user",
+    "secret_sauce",
+    "Epic sadface: Sorry, this user has been locked out.",
+  );
 });
